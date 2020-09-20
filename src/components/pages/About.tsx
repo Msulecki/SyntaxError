cb3108be438af475ecd4a288e655fab734769435
Mateusz Sułecki <mat.sulecki@gmail.com>
2020-09-20 12:27:55 +0200

    Add dot animation

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../../styles/pages/About.scss";
 
 export const About = () => {
+  const borderDot = useRef<HTMLDivElement>(null);
+
+  const handleDotAnimation = () => {
+    borderDot.current?.classList.add("about__header-border--animation");
+  };
+
   return (
-    <div id="about" className="about">
-      <div className="about__content">ABOUT</div>
-    </div>
+    <section id="about" className="about">
+      <div className="about__content">
+        <div className="about__header">
+          <h2>About</h2>
+          <div
+            ref={borderDot}
+            onClick={handleDotAnimation}
+            className="about__header-border"
+          ></div>
+        </div>
+      </div>
+    </section>
   );
 };
