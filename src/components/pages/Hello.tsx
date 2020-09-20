db9d081712784bbeea43478bea2dca3b7b73e28c
Mateusz Sułecki <mat.sulecki@gmail.com>
2020-09-20 12:28:06 +0200

    Add title to link

--- src/components/pages/Hello.tsx
+++ src/components/pages/Hello.tsx
@@ -2,24 +2,22 @@ import React from "react";
 import "../../styles/pages/Hello.scss";
 import { ReactComponent as Arrow } from "../../images/down_arrow.svg";
 
-export const Hello = () => {
-  return (
-    <section className="hello">
-      <div className="hello__wrapper">
-        <h1>
-          <span>Hi</span>
-          <span>,</span>
-          <span> I'm Matt</span>
-          <span>.</span>
-          <span> Webdeveloper</span>
-          <span>.</span>
-        </h1>
-        <div className="hello__arrow">
-          <a href="#about">
-            <Arrow className="hello__arrow--svg" />
-          </a>
-        </div>
+export const Hello = () => (
+  <section className="hello">
+    <div className="hello__wrapper">
+      <h1>
+        <span>Hi</span>
+        <span>,</span>
+        <span> I'm Matt</span>
+        <span>.</span>
+        <span> Webdeveloper</span>
+        <span>.</span>
+      </h1>
+      <div className="hello__arrow">
+        <a href="#about" title="Go to About page">
+          <Arrow className="hello__arrow--svg" />
+        </a>
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
