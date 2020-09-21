32f57cc4cd458d214c375db2faa8a0cd63973fae
Mateusz Sułecki <mat.sulecki@gmail.com>
2020-09-21 21:30:30 +0200

    Add maping for tags + Header component

--- src/components/pages/Stack.tsx
+++ src/components/pages/Stack.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { StackTag } from "../StackTag";
 import "../../styles/pages/Stack.scss";
+import { Header } from "../Header";
+import tags from "../../data/tags.json";
 
-export const Stack = () => {
-  return (
-    <section className="stack">
-      <div className="stack__column">
-        <div className="stack__header">
-          <h3>
-            Tech <span>&amp;</span> Tools
-          </h3>
-        </div>
-        <div className="stack__list">
-          <div className="stack__item">
-            <StackTag>React</StackTag>
-          </div>
-          <div className="stack__item">
-            <StackTag>React</StackTag>
-          </div>
-        </div>
+export const Stack = () => (
+  <section className="stack">
+    <div className="stack__column">
+      <div className="stack__header">
+        <Header title="Tech &amp; Tools" />
       </div>
-    </section>
-  );
-};
+      <div className="stack__list">
+        {tags.map((tag, index) => (
+          <div key={index} className="stack__item">
+            <StackTag>{tag}</StackTag>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+);
