6128804d4ce46b2c75bb5a0e7758555b2f5aa48c
Mateusz Sułecki <mat.sulecki@gmail.com>
2020-09-21 21:28:29 +0200

    Removed unnecessary return statement

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,12 +4,10 @@ import { About } from "./pages/About";
 import { Stack } from "./pages/Stack";
 import "../styles/Content.scss";
 
-export const Content = () => {
-  return (
-    <main>
-      <Hello />
-      <About />
-      <Stack />
-    </main>
-  );
-};
+export const Content = () => (
+  <main>
+    <Hello />
+    <About />
+    <Stack />
+  </main>
+);
