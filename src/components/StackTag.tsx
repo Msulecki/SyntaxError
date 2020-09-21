3c8a39d2ce46e37b23fb6747b678906161be8fe6
Mateusz Sułecki <mat.sulecki@gmail.com>
2020-09-21 21:29:28 +0200

    Changed tag styling

--- src/components/StackTag.tsx
+++ src/components/StackTag.tsx
@@ -3,11 +3,5 @@ import { StackTagProps } from "../interfaces/interfaces";
 import "../styles/StackTag.scss";
 
 export const StackTag = ({ children }: StackTagProps) => (
-  <div className="tag">
-    {children}
-    <div className="tag__border tag__border--lt"></div>
-    <div className="tag__border tag__border--rt"></div>
-    <div className="tag__border tag__border--ld"></div>
-    <div className="tag__border tag__border--rd"></div>
-  </div>
+  <div className="tag">{children}</div>
 );
