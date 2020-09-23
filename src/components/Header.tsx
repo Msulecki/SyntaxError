60256da0274694645e2a003ebbf254eb4b539300
Mateusz Sułecki <mat.sulecki@gmail.com>
2020-09-23 22:10:12 +0200

    Add key to mapped jsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,10 @@ export const Header = ({ title }: HeaderProps) => (
   <h2 className="header">
     {title.includes("&")
       ? title.split("&").map((text, index) => (
-          <>
+          <React.Fragment key={index}>
             {text}
             {title.split("&").length > index + 1 && <span>&amp;</span>}
-          </>
+          </React.Fragment>
         ))
       : title}
     <div className="header__border header__border--lt"></div>
